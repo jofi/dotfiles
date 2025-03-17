29ebffecab29cbdcfa0838ead6b848354572f098
Jozef Fulop <jozef.fulop@innovatrics.com>
2025-03-17 16:48:07 +0100

    Try eza aliases as replacement for ls

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -6,3 +6,13 @@ alias be='bundle exec'
 alias lic="cd ${WORKSPACE}/_license/license_generators/pc"
 alias te='export $(tmux showenv SSH_AUTH_SOCK)'
 alias fix-pom='mvn com.github.ekryd.sortpom:sortpom-maven-plugin:sort -Dsort.createBackupFile=false' 
+
+# eza
+alias ls='eza $eza_params'
+alias l='eza --git-ignore $eza_params'
+alias ll='eza --all --header --long $eza_params'
+alias llm='eza --all --header --long --sort=modified $eza_params'
+alias la='eza -lbhHigUmuSa'
+alias lx='eza -lbhHigUmuSa@'
+alias lt='eza --tree $eza_params'
+alias tree='eza --tree $eza_params'
